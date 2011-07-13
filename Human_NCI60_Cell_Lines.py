0be3e5b8e2864f0fe1065d31180f7b2fde6f3438
don19927 <donald.richardson@nyumc.org>
2011-07-13 11:49:21 -0700

    Edited Human_NCI60_Cell_Lines.py via GitHub

--- Human_NCI60_Cell_Lines.py
+++ Human_NCI60_Cell_Lines.py
@@ -7,14 +7,13 @@ conn = pymysql.connect(user='root',passwd="",db='bio_gps')
 cur = conn.cursor()
 
 
-insert_statement = open('/Users/Donnie/Desktop/NYU Research/Human data table/gnf1h.annot2007.csv', 'rb')
+insert_statement = csv.reader(open('/Users/Donnie/Desktop/NYU Research/Human data table/gnf1h.annot2007.csv', 'rb'))
 
-x = 1
-
-while x < 22259:
-	fob = insert_statement.readline(x)
-	cur.execute("INSERT INTO Human_GNF1H_Annotation VALUE(%s)",(fob))
-	x += 1
+for row in insert_statement:
+	print row
+	cur.execute("""INSERT INTO Human_GNF1H_Annotation  (ProbesetID, Num_matched_probes, RefSeq, 
+					UniGene, RIKEN, EntrezGene, Symbol, Description, Ensembl_representative, 
+					Cross_hyb_EntrezGeneIDs) VALUES(%s)""",(row))
 	
 
-	 
+#The the problem with this code is the sql VALUES statement treats the variable row as only one value.  so far I am only able to upload data into one column and not the entire table.  Having trouble getting python to recognize the values separately.
